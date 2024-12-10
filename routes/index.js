10ed0122ca56b3ae37f0459df168d0ade29b9fc4
ExtraPolite <imextrapolite2@gmail.com>
2024-12-10 15:36:49 -0500

    start project

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  try {
+    const db = req.app.locals.db;
+    const memories = await db
+      .collection('memories')
+      .aggregate([
+        {
+          $lookup: {
+            from: 'users',
+            foreignField: '_id',
+            localField: 'authorId',
+            as: 'author',
+          },
+        },
+      ])
+      .toArray();
+
+    console.log(memories);
+    res.render('index', { title: 'Express', memories: memories });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
