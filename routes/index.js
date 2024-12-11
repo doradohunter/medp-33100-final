8b1ba13a5eb07353b83cf46e7386622e71e80d96
Rochelle Williams <rochelle.williams80@myhunter.cuny.edu>
2024-12-11 16:35:25 -0500

    started adding backend

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,21 @@ router.get('/', async function (req, res, next) {
             foreignField: '_id',
             as: 'author'
           }
+        },
+        {
+          $lookup: {
+            from: 'platforms',
+            localField: 'platform',
+            foreignField: '_id',
+            as: 'platform'
+          }
         }
       ])
       .toArray();
-    res.render('index', { title: 'Gaming Journal', entries: entries });
+    const authors = await db.collection('authors').find().toArray();
+    const platforms = await db.collection('platforms').find().toArray();
+
+    res.render('index', { title: 'Gaming Journal', entries: entries, authors: authors, platforms: platforms});
   } catch (error) {
     console.log('error!');
     res.status(500).send('An error occurred');
